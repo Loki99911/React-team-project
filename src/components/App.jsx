73bb06c4f750f7d79031bd05d6c719d006fef1cd
Loki99911 <nikita_kresik@i.ua>
2023-01-18 17:53:42 +0200

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { lazy } from 'react';
 import { SharedLayout } from '../components/SharedLayout/SharedLayout';
 // import { AppLayout } from './App.styled';
 import { PublicRoute, PrivateRoute } from '../service/routes';
 import { useMediaRules } from '../MediaRules/MediaRules';
+import { refreshUser } from 'redux/Auth/authOperations';
 
 const Register = lazy(() => import('../pages/Register/Register'));
 const Main = lazy(() => import('../pages/Main/Main'));
@@ -13,7 +14,12 @@ const Expenses = lazy(() => import('../pages/Expenses/Expenses'));
 const Income = lazy(() => import('../pages/Income/Income'));
 const Reports = lazy(() => import('../pages/Reports/Reports'));
 
+
 export const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
   const { isMobile } = useMediaRules();
   return (
     <Routes>
