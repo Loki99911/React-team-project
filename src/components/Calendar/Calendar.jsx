3f603423e8d1144f823ce763914ac69ecc4be8d2
Serhii Vorotnoi <svorotnoy@gmail.com>
2023-01-18 15:00:05 +0200

    fix2

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-import s from './Calendar.module.css';
-import DatePicker from 'react-datepicker';
-import { ReactComponent as NewCalendar } from './images/calendar.svg';
-import 'react-datepicker/dist/react-datepicker.css';
-
-const Calendar = ({ date, setDate }) => {
-    return (
-        <div className={s.container}>
-            <NewCalendar className={s.icon} />
-            <DatePicker
-                className={s.calendar}
-                dateFormat="dd/MM/yyyy"
-                maxDate={new Date()}
-                selected={date}
-                onChange={date => setDate(date)}
-            />
-        </div>
-    );
-};
-
-export default Calendar;
-=======
 import DatePicker from 'react-datepicker';
 import { forwardRef } from 'react';
 import styles from '../Calendar/Calendar.module.css';
@@ -83,4 +60,3 @@ export const Calendar = ({ startDate, setStartDate, setDate }) => {
     </div>
   );
 };
->>>>>>> Stashed changes
