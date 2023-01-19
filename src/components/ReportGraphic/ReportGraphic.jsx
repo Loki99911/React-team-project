50abe2c1455b02ae09f7f7162b17bb6c31de60bf
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-01-19 21:30:54 +0200

    commit

--- src/components/ReportGraphic/ReportGraphic.jsx
+++ src/components/ReportGraphic/ReportGraphic.jsx
@@ -1,58 +1,58 @@
 import { TableBox, BoxPadding } from "./ReportGraphic.styled"
-import RenderShape from "components/RenderShape/RenderShape";
+// import RenderShape from "components/RenderShape/RenderShape";
 import { useEffect, useState} from "react";
 
 import React from "react";
 import { BarChart, Bar, LabelList, } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 1000,
-  },
-  {
-    name: "Page B",
-    uv: 2500,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 2290,
-    pv: 4300,
-    amt: 2100
-  }
-];
+// const data = [
+//   {
+//     name: "Page A",
+//     uv: 1000,
+//   },
+//   {
+//     name: "Page B",
+//     uv: 2500,
+//     pv: 1398,
+//     amt: 2210
+//   },
+//   {
+//     name: "Page C",
+//     uv: 2000,
+//     pv: 9800,
+//     amt: 2290
+//   },
+//   {
+//     name: "Page D",
+//     uv: 2780,
+//     pv: 3908,
+//     amt: 2000
+//   },
+//   {
+//     name: "Page E",
+//     uv: 1890,
+//     pv: 4800,
+//     amt: 2181
+//   },
+//   {
+//     name: "Page F",
+//     uv: 2390,
+//     pv: 3800,
+//     amt: 2500
+//   },
+//   {
+//     name: "Page G",
+//     uv: 2290,
+//     pv: 4300,
+//     amt: 2100
+//   }
+// ];
 
 const renderCustomBarLabel = ({x, y, width, value }) => {
   return <text x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>{`${value} UAH`}</text>;
 };
 
-const src = `M100,100 h200 a20,20 0 0 1 20,20 v200 a20,20 0 0 1 -20,20 h-200 a20,20 0 0 1 -20,-20 v-200 a20,20 0 0 1 20,-20 z`
+// const src = `M100,100 h200 a20,20 0 0 1 20,20 v200 a20,20 0 0 1 -20,20 h-200 a20,20 0 0 1 -20,-20 v-200 a20,20 0 0 1 20,-20 z`
 
 
 const getPath = (x, y, width, height) => {
