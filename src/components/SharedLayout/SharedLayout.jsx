8b6b3dd7b4c8f6e67363c7096b3fa2aeace265ca
MarianaDobrivska <dobrivska.marry@gmail.com>
2023-01-19 18:49:37 +0200

    add ToastContainer to SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-// import { Container } from './SharedLayout.styled';
+import { Container } from './SharedLayout.styled';
 import { AppBar } from '../AppBar/AppBar';
 import { Spinner } from 'components/Spinner/Spinner';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const SharedLayout = () => {
   return (
-    <div>
+    <Container>
       <AppBar />
       <Suspense fallback={<Spinner />}>
         <Outlet />
       </Suspense>
-    </div>
+      <ToastContainer />
+    </Container>
   );
 };
