6b805246061cacdf20268aa837b7b29bd58d4059
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-01-19 23:22:00 +0200

    Update ReportsData.jsx

--- src/components/ReportsData/ReportsData.jsx
+++ src/components/ReportsData/ReportsData.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import ReportStats from 'components/ReportStats/ReportStats';
 import ReportCategories from 'components/ReportCategories/ReportCategories';
