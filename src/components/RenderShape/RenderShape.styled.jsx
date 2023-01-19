829a1027598b7ae80ddb9a297919f0535ada7027
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-01-19 20:51:29 +0200

    friday

---
+++ src/components/RenderShape/RenderShape.styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrap = styled.path`
+width: ${({ width }) => width};
+height: ${({height}) => height};
+fill: ${({fill}) => fill};
+border-radius: 10px 10px 0px 0px;
+`
--- src/components/ReportGraphic/ReportGraphic.jsx
+++ src/components/ReportGraphic/ReportGraphic.jsx
@@ -1,4 +1,6 @@
 import { TableBox, BoxPadding } from "./ReportGraphic.styled"
+import RenderShape from "components/RenderShape/RenderShape";
+import { useEffect, useState} from "react";
 
 import React from "react";
 import { BarChart, Bar, LabelList, } from "recharts";
@@ -50,13 +52,34 @@ const renderCustomBarLabel = ({x, y, width, value }) => {
   return <text x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>{`${value} UAH`}</text>;
 };
 
-export default function ReportGraphic({ onChange }) {
+const src = `M100,100 h200 a20,20 0 0 1 20,20 v200 a20,20 0 0 1 -20,20 h-200 a20,20 0 0 1 -20,-20 v-200 a20,20 0 0 1 20,-20 z`
+
+
+const getPath = (x, y, width, height) => {
+ return `M${x},${y + height}
+          C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
+          C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+          Z`;}
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+
+export default function ReportGraphic({ state }) {
+  const [dataArray, setDataArray] = useState([]);
+
+ useEffect(() => {
+   return setDataArray(state)
+  }, [state])
+
   return (
       <BoxPadding>
           <TableBox>
-              <p>{onChange === "expenses"? `expenses graphic`: `income graphic`}</p>
-                <BarChart width={600} height={320} data={data} margin={{ top: 20, right: 0, bottom: 20, left: 0 }}>
-      <Bar dataKey="uv" fill="#FF751D" label={renderCustomBarLabel}  >
+                <BarChart width={600} height={320} data={dataArray} margin={{ top: 20, right: 0, bottom: 20, left: 0 }}>
+      <Bar dataKey="uv" fill="#FF751D" label={renderCustomBarLabel} shape={<TriangleBar />}  >
         <LabelList dataKey="name" position="bottom"  />
       </Bar>
     </BarChart>      
