1c85dddb88da51e420b92775902dad4c8f81e818
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-01-20 22:57:54 +0200

    Andrii

--- src/components/ReportGraphic/ReportGraphic.jsx
+++ src/components/ReportGraphic/ReportGraphic.jsx
@@ -3,7 +3,7 @@ import { TableBox, BoxPadding, Text } from "./ReportGraphic.styled"
 import { useEffect, useState} from "react";
 
 import React from "react";
-import { BarChart, Bar, LabelList, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, LabelList, ResponsiveContainer, Cell, ComposedChart, CartesianGrid,XAxis, YAxis } from "recharts";
 
 
 
@@ -23,10 +23,9 @@ import { BarChart, Bar, LabelList, ResponsiveContainer } from "recharts";
 //   return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
 // };
 
-// const renderCustomizedBottomLabel = (props) => {
-//   const { x, y, width, height, value } = props;
-//     return <Text x={x + width / 2} y={height} fill="#666" textAnchor="middle" dy={-6}>{`${value} UAH`}</Text>;
-// }
+const renderCustomizedBottomLabel = ({x, y, width, value }) => {
+  return <Text x={x*1.05 + width } y={y} fill="#666" textAnchor="middle" dy={-6}>{`${value} UAH`}</Text>;
+};
 
 const renderCustomBarLabel = ({x, y, width, value }) => {
   return <Text x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>{`${value} UAH`}</Text>;
@@ -56,32 +55,72 @@ export default function ReportGraphic({ state }) {
   return (
       <BoxPadding>
           <TableBox>
-        <ResponsiveContainer>
-                  <BarChart width={604} height={380} barCategoryGap={50} barGap={20} data={dataArray} margin={{ top: 20, right:20, bottom: 60, left: 20 }}>
-      <Bar barSize={38} dataKey="uv" fill="#FF751D" label={renderCustomBarLabel} >
-        <LabelList dataKey="name" position="bottom" angle="-15"  />
-      </Bar>
-    </BarChart>  
-    </ResponsiveContainer>
+<ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          layout="vertical"
+          width={280}
+          height={520}
+          data={dataArray}
+          margin={{
+            top: 20,
+            right: 40,
+            bottom: 20,
+            left: 40,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis type="number" />
+          <YAxis  type="none" />
+            <Bar dataKey="uv" barSize={15} fill="#413ea0" label={renderCustomizedBottomLabel}> 
+               <LabelList dataKey="name" position="top"  />
+              </Bar>
+        </ComposedChart>
+      </ResponsiveContainer>
         </TableBox>
     </BoxPadding>
   );
 }
 
+const colors = ['#FF751D', '#FFDAC0', '#FFDAC0','#FF751D', '#FFDAC0', '#FFDAC0', '#FF751D', '#FFDAC0', '#FFDAC0','#FF751D', '#FFDAC0', '#FFDAC0', '#FF751D', '#FFDAC0', '#FFDAC0', '#FF751D', '#FFDAC0', '#FFDAC0',];
 
-    //   <BoxPadding>
-    //       <TableBox>
-    //             <BarChart width={600} height={320} data={dataArray} margin={{ top: 20, right: 10, bottom: 40, left: 10 }}>
+
+{/* <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          layout="vertical"
+          width={280}
+          height={520}
+          data={dataArray}
+          margin={{
+            top: 20,
+            right: 40,
+            bottom: 20,
+            left: 40,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis type="number" />
+          <YAxis  type="none" />
+            <Bar dataKey="uv" barSize={15} fill="#413ea0" label={renderCustomizedBottomLabel}> 
+               <LabelList dataKey="name" position="top"  />
+              </Bar>
+        </ComposedChart>
+      </ResponsiveContainer> */}
+
+// "top" | "left" | "right" | "bottom" | "inside" | "outside" | "insideLeft" | "insideRight" | "insideTop" | "insideBottom" | "insideTopLeft" | "insideBottomLeft" | "insideTopRight" | "insideBottomRight" | "insideStart" | "insideEnd" | "end" | "center"
+
+
+{/*  */}
+
+
+
+    //         <ResponsiveContainer width="100%" height="100%">
+    //       <BarChart width={604} height={380} barCategoryGap="50%" barGap="50%" data={dataArray} margin={{ top: 20, right: 20, bottom: 60, left: 20 }}>
+    //                   <CartesianGrid stroke="#F5F6FB" height={2} vertical={false} horizontalPoints={[40, 75, 110, 145, 180, 215, 250, 285, 320]} width="100%"/>
     //   <Bar barSize={38} dataKey="uv" fill="#FF751D" label={renderCustomBarLabel} >
-    //     <LabelList dataKey="name" position="bottom" angle="-10" />
+    //           <LabelList dataKey="name" position="bottom" angle="-15" />
+    //   {dataArray.map((entry, index) => (
+    //       <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+    //     ))} 
     //   </Bar>
-    // </BarChart>      
-    //     </TableBox>
-    // </BoxPadding>
-
-// export default function ReportGraphic({ onChange }) {
-//     return (
-//         <><BoxPadding>
-//         <TableBox><p>{onChange === "expenses"? `expenses graphic`: `income graphic`}</p></TableBox></BoxPadding></>
-//     )
-// }
+    // </BarChart>  
+    // </ResponsiveContainer>
