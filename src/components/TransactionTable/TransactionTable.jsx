f95f7759091fa4b98867810b35cdb61cea129325
Serhii Vorotnoi <svorotnoy@gmail.com>
2023-01-20 18:15:38 +0200

    fix table

--- src/components/TransactionTable/TransactionTable.jsx
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -8,39 +8,49 @@ export const TransactionTable = ({ tablePage, transactionData }) => {
   const dispatch = useDispatch();
   console.log(tablePage);
 
-
   return (
 
+    <div className={s.TableScroll}>
 
-    <table className={s.Table}>
-      <thead className={s.Thead}>
-        <tr>
-          <th>Date</th>
-          <th>Description</th>
-          <th>Category</th>
-          <th>Sum</th>
-          <th></th>
-        </tr>
-      </thead>
 
-      <tbody>
-        {transactionData?.map(item => (
-          <tr key={item._id}>
-            <td className={s.Td}>{item.date}</td>
-            <td className={s.Description}>{item.description}</td>
-            <td className={s.Td}>{Translator(item.category)}</td>
-            {tablePage === '/main/expenses' || tablePage === '/expenses' || tablePage === '/Main/expenses' ? (
-              <td className={s.ExpensesSum}>{`- ${item.amount} грн.`}</td>
-            ) : (
-              <td className={s.IncomeSum}>{`${item.amount} грн.`}</td>
-            )}
-            <td className={s.Td}>
-              <DeleteButton onClick={() => dispatch(removeTransaction(item._id))} />
-            </td>
+
+      <table>
+        <thead className={s.Thead}>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Sum</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table >
+        </thead>
+      </table>
+      <div className={s.TableScrollBody}>
+        <table>
+          <tbody className={s.Tbody} >
+            {transactionData?.map(item => (
+              <tr className={s.Tr} key={item._id} colspan='4'>
+                <td className={s.Td} > {item.date}</td>
+                <td className={s.Description}>{item.description}</td>
+                <td className={s.Td}>{Translator(item.category)}</td>
+                {
+                  tablePage === '/main/expenses' || tablePage === '/expenses' || tablePage === '/Main/expenses' ? (
+                    <td className={s.ExpensesSum}>{`- ${item.amount} грн.`}</td>
+                  ) : (
+                    <td className={s.IncomeSum}>{`${item.amount} грн.`}</td>
+                  )
+                }
+                <td className={s.Td}>
+                  <DeleteButton onClick={() => dispatch(removeTransaction(item._id))} />
+                </td>
+              </tr >
+            ))}
+          </tbody >
+
+
+        </table >
+      </div >
+    </div >
   );
 };
 
