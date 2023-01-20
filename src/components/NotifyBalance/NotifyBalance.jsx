01eba761267446637e337abee2dac0bec455e981
AlenaUshakova <alenushakova@gmail.com>
2023-01-20 16:20:33 +0200

    Balance, Notify, Info,

--- src/components/NotifyBalance/NotifyBalance.jsx
+++ src/components/NotifyBalance/NotifyBalance.jsx
@@ -12,18 +12,18 @@ const body = document.querySelector('body');
 
 export default function NotifyBalance() {
   const [isModalOpen, setIsModalOpen] = useState(true);
-  // Handle close
+
   const handleModalClose = () => {
     setIsModalOpen(false);
     body.classList.remove('no-scroll');
   };
-  // Close on backdrop click
+
   const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
       handleModalClose();
     }
   };
-  // Add / Remove no-scroll class on body
+
   useEffect(() => {
     body.classList.add('no-scroll');
     return () => body.classList.remove('no-scroll');
