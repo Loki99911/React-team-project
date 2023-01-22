4217ed81649190ce4e22086e50c38c1e53f40650
AlenaUshakova <alenushakova@gmail.com>
2023-01-22 12:58:28 +0200

    Fixed and add new files

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -5,13 +5,8 @@ import { AppBar } from '../AppBar/AppBar';
 import { Loader } from 'components/Loader/Loader';
 import { ToastContainer } from 'react-toastify';
 import AniT from '../AniT/AniT';
-import { useSelector } from 'react-redux';
-import { getExpencesTransactions, getIncomesTransactions } from 'redux/Transaction/transactionSelectors';
-// import Ani from 'components/Ani/Ani';
 
 export const SharedLayout = () => {
-   const income = useSelector(getIncomesTransactions);
-   const expenses = useSelector(getExpencesTransactions);
   return (
     <Container>
       <AppBar />
@@ -20,7 +15,6 @@ export const SharedLayout = () => {
       </Suspense>
       <ToastContainer />
       <AniT color={'green'} />
-      {/* <Ani/> */}
     </Container>
   );
 };
