76d1bd89c7c66e7f6da5165d1bcbc64d2fad855e
Loki99911 <nikita_kresik@i.ua>
2023-08-14 10:14:38 +0300

    Update useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 
-export function useLocalStorage(key, defaultValue='light') {
-    const [state, setState] = useState(
-        () => JSON.parse(window.localStorage.getItem(key)) ?? defaultValue
-    );
+export function useLocalStorage(key, defaultValue = 'light') {
+  const [state, setState] = useState(() => {
+    console.log(JSON.parse(window.localStorage.getItem(key)));
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+  });
 
-    useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(state));
-    }, [key, state]);
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
 
-    return [state, setState];
+  return [state, setState];
 }
